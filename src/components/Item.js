159586d4a74ab12f0a3f91a595444e1712cf3458
yit <yit@localhost.localdomain>
2019-03-07 15:28:22 +0530

    :zap: Edited Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,9 +7,15 @@ class Item extends Component {
       <div>
         <div className="row">
             <div className="col-lg-4 col-md-6 col-sm-9">
-                <img src={sample_pic} alt=""  />
-                <h4>This is heading</h4>
-                <p>This is some content</p>
+                <div className="row">
+                    <div className="col-12 col-sm-4">
+                        <img src={sample_pic} alt=""  />
+                    </div>
+                    <div className="col-12 col-sm-8">
+                        <h4>This is heading</h4>
+                        <p>This is some content</p>
+                    </div>
+                </div>
             </div>
             <div className="col-lg-4 col-md-6 col-sm-9">
                 <h4>This is heading</h4>
